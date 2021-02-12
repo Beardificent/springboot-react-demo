e9f9b4a25eabdf3195222799d87d978f6c59709f
Beardificent <cis.magito@gmail.com>
2021-02-12 11:05:44 +0100

    added row mappers to convert data to a java object

--- src/main/java/com/example/demo/student/StudentDataAccessService.java
+++ src/main/java/com/example/demo/student/StudentDataAccessService.java
@@ -1,25 +1,36 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.UUID;
 
 @Service
 public class StudentDataAccessService {
 
-    public List<Student> selectAllStudents(){
-        return List.of(
-                new Student(UUID.randomUUID(),
-                        "Cis",
-                        "Magito",
-                        "cis.magito@gmail.com",
-                        Student.Gender.MALE),
-                new Student(UUID.randomUUID(),
-                        "Goku",
-                        "Son",
-                        "kakarot@gmail.com",
-                        Student.Gender.MALE)
-        );
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public StudentDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Student> selectAllStudents() {
+
+        String sql = "" + "SELECT student_id," + "first_name, " + "last_name, " + "email, " + "gender " + "FROM student";
+        return jdbcTemplate.query(sql, (resultSet, i) -> {
+            String studentId = resultSet.getString("student_id");
+            UUID studentIdUUID = UUID.fromString(studentId);
+            String first_name = resultSet.getString("first_name");
+            String last_name = resultSet.getString("last_name");
+            String email = resultSet.getString("email");
+            String genderString = resultSet.getString("gender").toUpperCase();
+            Student.Gender gender = Student.Gender.valueOf(genderString);
+            return new Student(studentIdUUID, first_name, last_name, email, gender);
+        });
+
     }
 }
